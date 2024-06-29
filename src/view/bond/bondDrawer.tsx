d64ccdd8ac3e8df7379f1cd57b2963000b1a0246
pichaochang <pichaochang@gmail.com>
2024-06-29 23:44:45 +0800

    fix

--- src/view/bond/bondDrawer.tsx
+++ src/view/bond/bondDrawer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as React from 'react';
 // import { MinusIcon, PlusIcon } from "@radix-ui/react-icons"
 // import { Bar, BarChart, ResponsiveContainer } from "recharts"
@@ -16,92 +17,46 @@ import {
   DrawerTrigger,
 } from '@/components/ui/drawer';
 import { useState } from 'react';
-export default function BondDrawaer() {
+import { Input } from '@/components/ui/input';
+export default function BondDrawaer(props:any) {
   const { t } = useTranslation();
   const [open, setOpen] = useState(false);
   const [btnLoading, setBtnLoading] = useState(false);
   return (
     // onOpenChange={setOpen}
-    <Drawer open={open} >
+    <Drawer open={open} onOpenChange={(val) => {
+      if (btnLoading) {
+        return;
+      }
+      setOpen(val);
+    }}>
       <DrawerTrigger asChild>
-        <LoadingButton loading={btnLoading} size='my' variant="my" onClick={() => {
-          setTimeout(() => {
-            setBtnLoading(true);
-            setOpen(true);
-            // setOpen(false)
-            setBtnLoading(false);
-          }, 3000);
-        }}>{t('stake.Approve')}</LoadingButton>
+        <LoadingButton className='w-full rounded-2xl'size='lg' variant="my" onClick={() => {
+        }}>{t('bond.BondPix')} {props.title}</LoadingButton>
       </DrawerTrigger>
       <DrawerContent className="dark:border-none">
         <div className="mx-auto w-full max-w-sm">
           <DrawerHeader>
-            <DrawerTitle>THS-USDT LP</DrawerTitle>
+            <DrawerTitle>{props.title}</DrawerTitle>
             {/* <DrawerDescription>Set your daily activity goal.</DrawerDescription> */}
           </DrawerHeader>
-          {/* <div className="p-4 pb-0">
-            <div className="flex items-center justify-center space-x-2">
-              <Button
-                variant="outline"
-                size="icon"
-                className="h-8 w-8 shrink-0 rounded-full"
-                onClick={() => onClick(-10)}
-                disabled={goal <= 200}
-              >
-                <span className="sr-only">Decrease</span>
-              </Button>
-              <div className="flex-1 text-center">
-                <div className="text-7xl font-bold tracking-tighter">
-                  {goal}
-                </div>
-                <div className="text-[0.70rem] uppercase text-muted-foreground">
-                  Calories/day
-                </div>
-              </div>
-              <Button
-                variant="outline"
-                size="icon"
-                className="h-8 w-8 shrink-0 rounded-full"
-                onClick={() => onClick(10)}
-                disabled={goal >= 400}
-              >
-                <span className="sr-only">Increase</span>
-              </Button>
-            </div>
-            <div className="mt-3 h-[120px]">
-              <ResponsiveContainer width="100%" height="100%">
-                <BarChart data={data}>
-                  <Bar
-                    dataKey="goal"
-                    style={
-                      {
-                        fill: "hsl(var(--foreground))",
-                        opacity: 0.9,
-                      } as React.CSSProperties
-                    }
-                  />
-                </BarChart>
-              </ResponsiveContainer>
-            </div>
-          </div> */}
-          <Skeleton></Skeleton>
+        
 
           <div>
-            <div>
-              <div>Bond Price</div>
-              <div>Market Price</div>
+            <div className='p-4'>
+              <Input className='mr-2 dark:border-none dark:bg-[#fff] dark:text-[#000]'></Input>
             </div>
           </div>
 
           <DrawerFooter className="mb-5">
             <div className="flex items-center justify-center">
-              <LoadingButton loading={btnLoading} size='my' variant="my" onClick={() => {
+              <LoadingButton loading={btnLoading}className='w-full rounded-2xl' size='lg' variant="my" onClick={() => {
                 setBtnLoading(true);
                 setTimeout(() => {
                   setBtnLoading(false);
                   // setOpen(false)
                 }, 3000);
-              }}>{t('stake.Approve')}</LoadingButton>
+              }}>{t('global.confirm')}</LoadingButton>
               {/* <DrawerClose asChild>
                 <Button variant="outline">Cancel</Button>
               </DrawerClose> */}
--- src/view/bond/index.tsx
+++ src/view/bond/index.tsx
@@ -4,6 +4,12 @@ import { useStores } from '@/store';
 import { useTranslation } from 'react-i18next';
 // import { Button } from '@/components/ui/button';
 import BondDrawaer from './bondDrawer';
+import { LoadingButton } from '@/components/ui/loadingButton';
+import THS  from '@/assets/common/THS.png';
+import USDT from  '@/assets/common/USDT.png';
+import WIN  from '@/assets/common/WIN.png';
+
+
 const Bond = () => {
   const { t } = useTranslation();
 
@@ -15,40 +21,46 @@ const Bond = () => {
       <div className='bg-[#fff] dark:bg-[#1d1d1b] rounded-lg p-2'>
 
         <div className='mb-3'>
-          <div className='text-[#AF27DF] text-md'>{t('stake.SingleStake')}</div>
+          <div className='text-[#AF27DF] dark:text-[#fff] text-md'>{t('bond.Bond')}</div>
         </div>
 
-        <div className='mb-3'>
-          <div className=' text-md'>{t('stake.APY')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>$ 888,888.8888</div>
-        </div>
-
-        <div className='mb-3'>
-          <div className=' text-md'>{t('stake.TotalDeposited')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>$ 888,888.8888</div>
-        </div>
-
-        <div className='mb-3'>
-          <div className=' text-md'>{t('stake.CurrentIndex')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>$ 888,888.8888</div>
-        </div>
-
-        <div className='mt-6 mb-3 text-center'>
-          <div className='text-xs text-[#ababab]'>{t('stake.title')}</div>
-          <div className='text-xs text-[#ababab]'>{t('stake.title2')}</div>
-        </div>
-
-        <div className='text-center'>
-          <BondDrawaer></BondDrawaer>
+        <div className='flex items-center justify-between py-4'>
+          <div className='flex justify-center flex-col items-center mb-3 w-1/2'>
+            <div className='text-md mb-2'>$2640.954</div>
+            <div className='text-xs text-[#ababab] dark:text-[#7d7d7c]'>{t('bond.THSPrice')}</div>
+          </div>
+          <div className='flex justify-center flex-col items-center mb-3 w-1/2'>
+            <div className='text-md mb-2'>$2640.954</div>
+            <div className='text-xs text-[#ababab] dark:text-[#7d7d7c]'>{t('bond.TOTALPRICE')}</div>
+          </div>
         </div>
       </div>
 
-      <div className='bg-[#fff] dark:bg-[#1d1d1b] rounded-lg p-2 mt-3'>
-        <div className=''>
-          <div className='text-[#AF27DF] text-md'>{t('stake.StakeList')}</div>
-          <div className=' text-center text-xs text-[#ababab] dark:text-[#2a2a28]'>
-            <span>{t('stake.noStake')}</span>
+      {/* bond */}
+      <div  className='text-xs flex flex-col  bg-[#fff] dark:bg-[#1d1d1b] rounded-lg p-2 mt-3'>
+        <div className='flex items-center my-1'>
+          <div className='flex items-center'>
+            <img className='h-5 w-5' src={THS} alt="" />
+            <img className='h-5 w-5' src={USDT} alt="" />
           </div>
+          <div className='ml-2'>
+            THS-USDT LP
+          </div>
+        </div>
+        <div className='flex items-center justify-between my-1'>
+          <span>{t('bond.PRICE')}</span>
+          <span>$18.59</span>
+        </div>
+        <div className='flex items-center justify-between my-1'>
+          <span>{t('bond.APY')}</span>
+          <span>22%</span>
+        </div>
+        <div className='flex items-center justify-between my-1'>
+          <span>{t('bond.PurchaseQuantity')}</span>
+          <span>18.59</span>
+        </div>
+        <div className='w-full text-center mt-1'>
+          <BondDrawaer title="THS-USDT LP"></BondDrawaer>
         </div>
       </div>
     </div>
