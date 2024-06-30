9701bc6c9c01df84ed7b2af1e1d8dba1b61b7188
pichaochang <pichaochang@gmail.com>
2024-06-30 23:38:53 +0800

    fix

--- src/utils/commonSdk.ts
+++ src/utils/commonSdk.ts
@@ -7,6 +7,8 @@ import { ERC20Abi } from './abi';
 import { getUUID } from './utils';
 import toast from 'react-hot-toast';
 import i18n from '@/react-i18next-config';
+import Web3 from 'web3';
+import { erc20Abi } from 'viem';
 const t = i18n.t;
 const {pexAddress, spexAddress, winAddress, usdbAddress, lpAddress, sbAddress, rpc } =  contractProdAddress;
 console.log('pexAddress', pexAddress);
@@ -19,6 +21,9 @@ console.log('sbAddress', sbAddress);
 console.log('rpc', rpc);
 export const getUserInfo  = async () => {
   // todo
+};
+const getGasBase = (num:any) => {
+  return String(BigInt(num) * 110n / 100n);
 };
 export const loginWallet = async () => {
   const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
@@ -45,4 +50,37 @@ export const globalVaild = async () => {
 //   const signer = provider.getSigner();
 //   const contract = new ethers.Contract(chainInfo.value.ModeAddress, abi, signer);
 };
-
+export const fn = async () => {
+  try {
+    const web3 = new Web3(window.ethereum);
+    const getAccounts = await web3.eth.getAccounts();
+    const account = getAccounts[0];
+    if (!account) {
+      return Promise.resolve(false);
+    }
+    const myContract = new web3.eth.Contract(erc20Abi, usdbAddress);
+    const functionCall = myContract.methods.pumpMasterMint(account);
+    // 获取gasPrice
+    const gasPrice:any = await web3.eth.getGasPrice();
+    // 获取gas
+    const gasBase = await functionCall.estimateGas({ gasPrice, from: account, });
+    const gas = getGasBase(gasBase);
+    console.log('gas-gasPrice', gas, gasPrice);
+    // 预执行 result
+    const result = await functionCall.call({ gasPrice, gas, from: account });
+    if (!result) {
+      console.log('result', result);
+      return Promise.resolve(false);
+    }
+    const transaction = await functionCall.send({ gasPrice, gas, from: account });
+    console.log('transactionHash', transaction);
+    if (transaction.transactionHash) {
+      return Promise.resolve(transaction);
+    } else {
+      return Promise.resolve(false);
+    }
+  } catch (err) {
+    console.log('err', err?.message);
+    return Promise.resolve(false);
+  }
+};
