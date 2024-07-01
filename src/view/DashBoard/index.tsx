de383fc15427179dac66f40ec8250498136f4234
pichaochang <pichaochang@gmail.com>
2024-07-01 11:43:27 +0800

    fix

--- src/view/DashBoard/index.tsx
+++ src/view/DashBoard/index.tsx
@@ -21,27 +21,27 @@ const DashBoard = () => {
 
         <div className='mb-3'>
           <div className='text-[#AF27DF] text-md'>{t('dashBoard.MarketCap')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>$ 888,888.8888</div>
+          <div className='text-xs text-[#ababab] dark:text-[#939393]'>$ 888,888.8888</div>
         </div>
 
         <div className='mb-3'>
           <div className='text-[#AF27DF] text-md'>{t('dashBoard.PEXPrice')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>$ 888,888.8888</div>
+          <div className='text-xs text-[#ababab] dark:text-[#939393]'>$ 888,888.8888</div>
         </div>
 
         <div className='mb-3'>
           <div className='text-[#AF27DF] text-md'>{t('dashBoard.BackingPerPEx')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>$ 888,888.8888</div>
+          <div className='text-xs text-[#ababab] dark:text-[#939393]'>$ 888,888.8888</div>
         </div>
 
         <div className='mb-3'>
           <div className='text-[#AF27DF] text-md'>{t('dashBoard.TotalMintin')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>$ 888,888.8888</div>
+          <div className='text-xs text-[#ababab] dark:text-[#939393]'>$ 888,888.8888</div>
         </div>
 
         <div className='mb-3'>
           <div className='text-[#AF27DF] text-md'>{t('dashBoard.winPrice')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>$ 888,888.8888</div>
+          <div className='text-xs text-[#ababab] dark:text-[#939393]'>$ 888,888.8888</div>
         </div>
 
       </div>
@@ -49,7 +49,7 @@ const DashBoard = () => {
       <div className='bg-[#fff] dark:bg-[#1d1d1b] rounded-lg p-2 mt-3'>
         <div className=''>
           <div className='text-[#AF27DF] text-md'>{t('dashBoard.title1')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>
+          <div className='text-xs text-[#ababab] dark:text-[#939393]'>
             <span>$ 888,888.8888</span>
             <span></span>
           </div>
@@ -63,7 +63,7 @@ const DashBoard = () => {
       <div className='bg-[#fff] dark:bg-[#1d1d1b] rounded-lg p-2 mt-3'>
         <div className=''>
           <div className='text-[#AF27DF] text-md'>{t('dashBoard.title1')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>
+          <div className='text-xs text-[#ababab] dark:text-[#939393]'>
             <span>$ 888,888.8888</span>
             <span></span>
           </div>
@@ -76,7 +76,7 @@ const DashBoard = () => {
       <div className='bg-[#fff] dark:bg-[#1d1d1b] rounded-lg p-2 mt-3'>
         <div className=''>
           <div className='text-[#AF27DF] text-md'>{t('dashBoard.title2')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>
+          <div className='text-xs text-[#ababab] dark:text-[#939393]'>
             <span>$ 888,888.8888</span>
             <span></span>
           </div>
@@ -90,7 +90,7 @@ const DashBoard = () => {
       <div className='bg-[#fff] dark:bg-[#1d1d1b] rounded-lg p-2 mt-3'>
         <div className=''>
           <div className='text-[#AF27DF] text-md'>{t('dashBoard.title3')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>
+          <div className='text-xs text-[#ababab] dark:text-[#939393]'>
             <span>$ 888,888.8888</span>
             <span></span>
           </div>
@@ -104,7 +104,7 @@ const DashBoard = () => {
       <div className='bg-[#fff] dark:bg-[#1d1d1b] rounded-lg p-2 mt-3'>
         <div className=''>
           <div className='text-[#AF27DF] text-md'>{t('dashBoard.title4')}</div>
-          <div className='text-xs text-[#ababab] dark:text-[#2a2a28]'>
+          <div className='text-xs text-[#ababab] dark:text-[#939393]'>
             <span>$ 888,888.8888</span>
             <span></span>
           </div>
